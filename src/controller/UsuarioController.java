a091fb58ef6e23b3cf90b1f4a3f44ac83c228ac5
nathan_silva150 <nathan_silva150@estudante.sesisenai.org.br>
2026-05-15 19:17:06 -0300

    Correção do UsuarioController

--- src/controller/UsuarioController.java
+++ src/controller/UsuarioController.java
@@ -1,4 +1,13 @@
 package controller;
 
+import model.service.UsuarioService;
+
 public class UsuarioController {
+    private UsuarioService service;
+
+    public UsuarioController(UsuarioService service) {
+        this.service = service;
+    }
+
+
 }
